c270cf8b46ad94242c9e4179bba75daa48918ad3
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-04-20 20:54:14 +0530

    change the section to div

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ document.addEventListener('readystatechange', (event) => {
 const initApp = () => {
     const firstPage = document.querySelector('.first-page');
     const secondPage = document.querySelector('.second-page');
-    const ratingSection = firstPage.querySelector('section');
+    const ratingSection = firstPage.querySelector('div');
     const finalRating = document.getElementById("final-rating");
     const submitEl = document.querySelector('.first-page>button');
 
